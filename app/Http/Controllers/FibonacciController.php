3ce455f908767cfeec66e215ec327d4572c43d77
Juan <juandavidlopez@gmail.com>
2023-01-18 15:46:16 -0500

    logica_falta_organizar_que_se_muestre_en_blade

--- app/Http/Controllers/FibonacciController.php
+++ app/Http/Controllers/FibonacciController.php
@@ -10,27 +10,30 @@ class FibonacciController extends Controller
         return view ('fibonacci');
     }
     public function fibocre(Request $request){
-        $a = 0;
-        $b = 1;
-        $c = 0;
-        $i = 1;
         $fibonacci=$request->fibonacci;
-        $resultado =$this->recur($fibonacci);
-        //while ($i <= $fibonacci){
-        //    $resultado .= $c.",";
-        //    $c = $a+$b;
-        //    $a = $b;
-        //    $b = $c;
-        //    $i++;
-        //}
+        $resultado=$this->proceso($fibonacci);
+    
         return view ('fibonacci', ['fibonacci'=>$resultado]);
     }
-    public function recur($fibonacci){
-        if ($fibonacci == 0 or $fibonacci == 1) {
-            $resultado=$fibonacci;
-        } else {
-            $resultado=$this->recur($fibonacci-1)+ $this->recur($fibonacci-2);
-        }
+    public function proceso($fibonacci){
+      for ($i=0; $i < $fibonacci ; $i++) { 
+        $resultado=$this->recur($i).",";
+        echo $resultado;
+      }
+    }
+    public function recur($fibonacci){($fibonacci == 0 or $fibonacci == 1) ? $resultado=$fibonacci: $resultado=$this->recur($fibonacci -1)+$this->recur($fibonacci-2);
         return $resultado;
     }
 }
+    
+//$a = 0;
+//$b = 1;
+//$c = 0;
+//$i = 1;
+//while ($i <= $fibonacci){
+    //    $resultado .= $c.",";
+    //    $c = $a+$b;
+    //    $a = $b;
+    //    $b = $c;
+    //    $i++;
+    //}
